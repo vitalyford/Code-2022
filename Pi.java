492d4068a94f04bfd39d5fe89dde21e961543931
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2022-09-07 10:02:21 -0400

    first commit

---
+++ Pi.java
@@ -0,0 +1,10 @@
+public class Pi {
+    public double sum() {
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        Pi p = new Pi();
+        System.out.println(p.sum());
+    }
+}
---
+++ mark.md
@@ -0,0 +1,9 @@
+# This is an example
+
+## Let's try the second level header as well
+
+- Let's see
+- How the list works
+
+1. Now let's see the enum
+2. 
