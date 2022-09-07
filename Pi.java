93833c3e5fd25e03357b0daf6833197d21d2b24e
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2022-09-07 10:19:14 -0400

    Implement pi calculation with recursion. Do not run it at home.

--- Pi.java
+++ Pi.java
@@ -1,10 +1,9 @@
 public class Pi {
-    public double sum() {
-        return 0;
-    }
-    
+    public double sum(int k) {
+        return (k < 0) ? 0 : Math.pow(-1.0, k) / ((2*k + 2) * (2*k + 3) * (2*k + 4)) + sum(k - 1);
+    }    
     public static void main(String[] args) {
         Pi p = new Pi();
-        System.out.println(p.sum());
+        System.out.println(3 + 4 * p.sum(10000));
     }
 }
