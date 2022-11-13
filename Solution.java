f561a6ce17206b2e00f301a585572f7cec71a760
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2022-11-12 21:46:49 -0500

    Complete leet code pizza challenge

--- Solution.java
+++ Solution.java
@@ -1,12 +1,12 @@
+import java.util.*;
+
 public class Solution {
-
     private static boolean checkApplesColCut(String[] pizza, int sR, int sC, int eR, int eC, int col) {
-
         // Checking the top part
         boolean leftCheck = false;
-        for (String p : pizza) {
+        for (int r = sR; r <= eR; r++) {
             for (int c = sC; c < col; c++) {
-                if (p.charAt(c) == 'A') {
+                if (pizza[r].charAt(c) == 'A') {
                     leftCheck = true;
                     break;
                 }
@@ -18,9 +18,9 @@ public class Solution {
 
         // Checking the bottom part
         boolean rightCheck = false;
-        for (String p : pizza) {
+        for (int r = sR; r <= eR; r++) {
             for (int c = col; c <= eC; c++) {
-                if (p.charAt(c) == 'A') {
+                if (pizza[r].charAt(c) == 'A') {
                     rightCheck = true;
                     break;
                 }
@@ -32,82 +32,82 @@ public class Solution {
 
         return leftCheck && rightCheck;
     }
-    
-    private static int count = 0;
 
     private static boolean checkApplesRowCut(String[] pizza, int sR, int sC, int eR, int eC, int row) {
-
         // Checking the top part
         boolean topCheck = false;
         for (int r = sR; r < row; r++) {
-            for (int i = sC; i <= eC; i++) {
-                if (pizza[r].charAt(i) == 'A') {
+            for (int c = sC; c <= eC; c++) {
+                if (pizza[r].charAt(c) == 'A') {
                     topCheck = true;
-                    count = r;
                     break;
                 }
+            }
+            if (topCheck) {
+                break;
             }
         }
 
         // Checking the bottom part
         boolean bottomCheck = false;
         for (int r = row; r <= eR; r++) {
-            for (int i = r; i <= eR; i++) {
-                if (pizza[r].charAt(i) == 'A') {
+            for (int c = sC; c <= eC; c++) {
+                if (pizza[r].charAt(c) == 'A') {
                     bottomCheck = true;
                     break;
                 }
+            }
+            if (bottomCheck) {
+                break;
             }
         }
 
         return topCheck && bottomCheck;
     }
 
-    private static int helper(String[] pizza, int k, int sR, int sC, int eR, int eC) {
+    // memoization
+    private static long helper(String[] pizza, int k, int sR, int sC, int eR, int eC, Map<String, Long> memo) {
+        String s = sR + " " + sC + " " + k;
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
 
         if (k == 1) {
             return 1; // it's one way to cut a pizza into k pieces
         }
-
-        int totalWays = 0,
-            num1 = -1;
-
+        long totalWays = 0;
         // Cutting horizontally
-        // sR = 0, eR = 2
-        // row = 1..2
         for (int row = sR + 1; row <= eR; row++) {
-            if ((totalWays - num1) > 1) {
-                sR = count + 1;
-                num1 += 1;
-            }
             if (checkApplesRowCut(pizza, sR, sC, eR, eC, row)) {
-                totalWays += helper(pizza, k - 1, row, sC, eR, eC);
-                sR += 1;
+                totalWays += helper(pizza, k - 1, row, sC, eR, eC, memo) % (long)(1E9 + 7);
             }
         }
 
         // Cutting vertically
         for (int col = sC + 1; col <= eC; col++) {
             if (checkApplesColCut(pizza, sR, sC, eR, eC, col)) {
-                totalWays += helper(pizza, k - 1, sR, col, eR, eC);
+                totalWays += helper(pizza, k - 1, sR, col, eR, eC, memo) % (long)(1E9 + 7);
             }
         }
 
-        return totalWays % (int)(10E9 + 7);
+        memo.put(s, totalWays % (long)(1E9 + 7));
+
+        return totalWays % (long)(1E9 + 7);
     }
 
     public static int ways(String[] pizza, int k) {
-
         // ["A..","AAA","..."]
         /* charAt()
         A . .
         A A A
         . . .
         */
-        return helper(pizza, k, 0, 0, pizza.length - 1, pizza[0].length() - 1);
+        return (int)helper(pizza, k, 0, 0, pizza.length - 1, pizza[0].length() - 1, new HashMap<>());
     }
 
     public static void main(String[] args) {
-        System.out.println(ways(new String[]{"A..","AAA","..."}, 3));
+        String[] pizza = {"A..","AAA","..."};
+        int k = 3;
+        System.out.println(ways(pizza, k));
     }
 }
